de881420caf13545f3123880e4b425d1172dd8f6
Slv-WebTech <sanyam.jain@pboplus.org>
2026-05-25 00:02:37 +0530

    Refactor: Comment out initial variable declarations and console logs in Algorithms.js; update index.html to clarify string and number addition behavior

--- Algorithms.js
+++ Algorithms.js
@@ -1,7 +1,10 @@
-let a = 10;
-let b = 40;
+// let a = 10;
+// let b = 40;
 
-console.log(a);
-console.log(b);
-console.log(a + b);
-console.log("the sum of a and b is " + a + b);
+// console.log(a);
+// console.log(b);
+// console.log(a + b);
+// console.log("the sum of a and b is " + a + b);
+
+// ----------------------------------------------------------------------------------
+
